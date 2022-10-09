5451e861f412b03c0822fbf148b01f96e7d0eb07
Max-Morozynskyi <m0r0zin@ukr.net>
2022-10-09 20:21:47 +0300

    homework done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// import * as basicLightbox from 'basiclightbox'
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems.map(itemMarkup).join("");
+galleryRef.innerHTML = galleryMarkup;
+// Markup creating end
+
+galleryRef.addEventListener("click", getBigImg)
+
+// All functions:
+
+function itemMarkup ({ preview, original, description }) {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+  <img
+  class="gallery__image"
+  src="${preview}"
+  data-source="${original}"
+  alt="${description}"
+  />
+  </a>
+  </div>`};
+  
+function getBigImg(evt) {
+  if (evt.target.nodeName !== "IMG") { return; }
+  evt.preventDefault();
+  let currentLargeImg = evt.target.dataset.source;
+  const modalWindow = basicLightbox.create(`
+      <img src="${currentLargeImg}" width="800" height="600">
+  `);
+  modalWindow.show()
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,14 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems.map(itemMarkup).join("");
+galleryRef.innerHTML = galleryMarkup;
+
+function itemMarkup ({ preview, original, description }) {
+  return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`};
+
+let lightbox = new SimpleLightbox('.gallery a', {  captionsData:"alt", captionDelay:250  });
